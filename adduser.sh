aef9931d41ea066846a4d7f7d4f51c58a6ff18c9
galifer <1159727000@qq.com>
2017-06-14 22:15:42 +0800

    upload

---
+++ adduser.sh
@@ -0,0 +1,8 @@
+npm install
+git config user.name "galifer"
+git config user.email "1159727000@qq.com"
+hexo g
+hexo d
+cd .deploy_git
+git config user.name "galifer"
+git config user.email "1159727000@qq.com"
---
+++ themes-download.sh
@@ -0,0 +1,6 @@
+#git clone https://github.com/iissnan/hexo-theme-next.git themes/next
+#git clone https://github.com/klugjo/hexo-theme-alpha-dust.git themes/alpha-dust
+#git clone https://github.com/MOxFIVE/hexo-theme-yelee.git themes/yelee
+#git clone https://github.com/galifer/hexo-theme-material.git themes/material
+#git clone https://github.com/tufu9441/maupassant-hexo.git themes/maupassant
+#git clone https://github.com/hexojs/hexo-theme-light.git themes/light
